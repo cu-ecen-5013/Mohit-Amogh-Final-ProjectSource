f899607a472675da34ae2e73611ade67188c95d3
MohitRane8 <mohit.rane@colorado.edu>
2019-12-01 20:27:28 -0700

    edited uart code to test incoming character from tiva

--- test_src/basic_uart/basic_uart.c
+++ test_src/basic_uart/basic_uart.c
@@ -12,6 +12,9 @@ int main(void)
     int fd, cnt;
     struct termios options;
 
+    // printf("Basic UART test\n");
+    printf("TEST WITH TIVA - RECEIVE TIVA DATA\n");
+
     if ((fd = open("/dev/ttyO1", O_RDWR | O_NOCTTY)) < 0)
     {
         perror("open\n");
@@ -37,26 +40,24 @@ int main(void)
     options.c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
 
     tcsetattr(fd, TCSAFLUSH, &options);
-    
-    printf("Basic UART test\n");
 
     // if(fork() == 0)     // parent
     // {
-        printf("[P]: Sending string\n");
+        // printf("[P]: Sending string\n");
 
-        // send string
-        unsigned char string_tx[13] = "Hello World!";
-        printf("[P]: sending-> '%s'\n", string_tx);
-        if ((cnt = write(fd, &string_tx, 13)) < 0)
-        {
-            perror("[P]: write\n");
-            return -1;
-        }
+        // // send string
+        // unsigned char string_tx[13] = "Hello World!";
+        // printf("[P]: sending-> '%s'\n", string_tx);
+        // if ((cnt = write(fd, &string_tx, 13)) < 0)
+        // {
+        //     perror("[P]: write\n");
+        //     return -1;
+        // }
     // }
     // else                // child
     // {
         // let parent transmit the string before receiving
-        usleep(100000);
+        usleep(10000000);
 
         printf("[C]: Receiving string\n");
 
